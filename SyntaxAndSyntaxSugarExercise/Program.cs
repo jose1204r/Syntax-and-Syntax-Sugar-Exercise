43d3f5c81e8b6a8bef9c21a0cfc9aff18deda1a3
jose <jrodriguez@itsolutionspcllc.com>
2022-10-15 18:19:23 -0400

    Syntax-and-Syntax-Sugar-Exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // int answer = 4; //***explicit typing ***
+            // string response;//***explicit typing ***
+
+            // if (answer < 9)
+            // {
+            // response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            ///  response = answer + “greater than or equal to nine”;
+            // }
+
+
+            //****** Explicit typing, Inferred Typing,Ternary Operator & String Interpolation*****
+
+            var answer  = 4; //Inferred Typing     
+            int number = 9; //Explicit typing
+            var response = (answer < number) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine";
+
+                
+            Console.WriteLine(response);
+
+
+
+
+
+
         }
     }
 }
